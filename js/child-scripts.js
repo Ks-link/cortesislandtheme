3e645bd555114fa9c2bd48d0ad5078027fbb1b27
Kaleb Link <kaleb.s.link@gmail.com>
2025-10-20 14:31:09 -0700

    add js file

--- js/child-scripts.js
+++ js/child-scripts.js
@@ -1,10 +1,1 @@
 // console.log("Hello Child Theme World!");
-
-let eventWidget = document.querySelector('#secondary .tribe-compatibility-container');
-let headerButton = document.querySelector('.header-custom-button');
-
-// add widget class to event widget
-eventWidget.classList.add('widget');
-
-// remove target blank attribute from header button so it opens in the same page
-headerButton.removeAttribute('target');
